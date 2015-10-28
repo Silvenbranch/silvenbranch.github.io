4a8311933fe9c29d5615e920832cd50616dd9e7a
Nathen <nathen.sample@logscape.com>
2015-10-28 14:14:55 +0000

    removed dayspan from warbands timer (as warbands happen every 7 hours)

--- js/warbands_timer.js
+++ js/warbands_timer.js
@@ -15,7 +15,7 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
+  //var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
